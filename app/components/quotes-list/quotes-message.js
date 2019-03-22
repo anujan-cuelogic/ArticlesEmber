ea5676ba14cd1ae0d0579a324bb6523a706a8775
Anuja Nagolkar <anuja.nagolkar@cuelogic.com>
2019-03-22 16:25:07 +0530

    Removed debugging statements

--- app/components/quotes-list/quotes-message.js
+++ app/components/quotes-list/quotes-message.js
@@ -4,8 +4,6 @@ export default Component.extend({
   store: Ember.inject.service(),
   actions: {
     destroyArticle(articleId) {
-      console.log('updateArticle', 'eeeeeeeeeeee')
-      console.log('updateArticle', 'eeeeeeeeeeee', articleId)
       this.get('store').findRecord('article', articleId, {backgroundReload: false}).then(function(article) {
         article.destroyRecord();
       });
--- app/components/quotes-update.js
+++ app/components/quotes-update.js
@@ -5,7 +5,6 @@ export default Component.extend({
   actions: {
 
     postArticle: function() {
-      console.log('postArticle ppppppppppppp', this.get('userId'))
       var txt = this.get('articleText');
       this.get('store').findRecord('user', this.get('userId')).then((user) => {
         var ArticleData = {body: txt, user: user};
@@ -17,8 +16,6 @@ export default Component.extend({
 
 
     updateArticle: function() {
-      console.log('updateArticle', 'eeeeeeeeeeee')
-      console.log('updateArticle', 'eeeeeeeeeeee', this.get('articleId'))
       var txt = this.get('articleText');
       this.get('store').findRecord('article', this.get('articleId')).then((article) => {
         article.set('body', txt);
