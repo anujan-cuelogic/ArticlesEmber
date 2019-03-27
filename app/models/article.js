faebb04f66560b3bf8c9c3df5d9184fb796d4691
Pravin Lokhande <pravin.lokhande@cuelogic.com>
2019-03-27 19:34:50 +0530

    article request minimised and login, register page redirected to index.

--- app/models/article.js
+++ app/models/article.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 import EmberObject, { computed } from '@ember/object';
 
 export default DS.Model.extend({
-  user: DS.belongsTo('user', {asyn: true}),
+  user: DS.belongsTo('user', {async: true}),
   createdDate: DS.attr('date', {default: new Date()} ),
   body: DS.attr('string')
 });
